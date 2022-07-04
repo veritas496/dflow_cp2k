bcbe2b615597986bb585ec654404245ca0b38b2a
Yani Guan <56317222+veritas496@users.noreply.github.com>
2022-07-04 16:36:34 +0800

    Add files via upload

---
+++ dflow_3steps_cp2k_slurm.py
@@ -0,0 +1,108 @@
+from dflow import SlurmRemoteExecutor
+
+from dflow import (
+    upload_artifact,
+    Workflow,
+    Step
+)
+
+from dflow.python import (
+    PythonOPTemplate,
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact, 
+    Slices
+)
+import os
+from typing import List
+from pathlib import Path
+import subprocess
+
+class CP2KOpt(OP):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            "Opt_input": Artifact(Path)
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            "Opt_output": Artifact(Path)
+        })
+
+    @OP.exec_sign_check
+    def execute(self, op_in: OPIO) -> OPIO:
+        cwd = os.getcwd()
+        os.chdir(op_in["Opt_input"]) 
+        cmd = "ulimit -s unlimited; srun /opt/cp2k-9.1/cp2k-9.1-Linux-x86_64.ssmp -i input.inp -o output.out"
+        subprocess.call(cmd, shell=True)
+        os.chdir(cwd)        
+        return OPIO({
+            "Opt_output": op_in["Opt_input"]/"output.out"
+        })
+
+class CP2KSingle(OP):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_input_sign(cls):
+        return OPIOSign({
+            'Single_input': Artifact(Path),
+        })
+
+    @classmethod
+    def get_output_sign(cls):
+        return OPIOSign({
+            "Single_output": Artifact(Path),
+        })
+
+    @OP.exec_sign_check
+    def execute(self, op_in: OPIO) -> OPIO:
+        cwd = os.getcwd()
+        os.chdir(op_in["Single_input"])
+        cmd = "ulimit -s unlimited; srun /opt/cp2k-9.1/cp2k-9.1-Linux-x86_64.ssmp -i input.inp -o output.out"
+        subprocess.call(cmd, shell=True)
+        os.chdir(cwd)
+        return OPIO({
+            "Single_output": op_in["Single_input"]/"output.out"
+        })
+
+def main():
+    slurm_remote_executor = SlurmRemoteExecutor(
+    host="39.99.247.57", 
+    port=22, 
+    username="tutor",  
+    password="Bohrium#chengdu", 
+    header="#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks-per-node=4\n#SBATCH --partition=cpu\n#SBATCH -e test.err\n", 
+    )
+
+    Structure_Opt = Step(
+        "Structure-Opt",
+        PythonOPTemplate(CP2KOpt, image="dptechnology/dflow"),
+        artifacts={"Opt_input": upload_artifact(["./cp2k_opt"])},
+        executor=slurm_remote_executor,
+    )
+
+    CP2K_Electronic = Step(
+        "CP2K-Electronic",
+        PythonOPTemplate(CP2KSingle, image="dptechnology/dflow", slices=Slices("{{item}}", input_artifact=["Single_input"], output_artifact=["Single_output"])),
+        artifacts={"Single_input": upload_artifact(["cp2k_elf", "cp2k_density"])}, # your input dir
+        with_param=range(2),
+        key="CP2KSingle-{{item}}",
+        executor=slurm_remote_executor,
+    )
+
+    wf = Workflow(name="cp2k-task")
+    wf.add(Structure_Opt)
+    wf.add(CP2K_Electronic)
+    wf.submit()
+
+if __name__ == "__main__":
+    main()
+
