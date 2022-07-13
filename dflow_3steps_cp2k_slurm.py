3e513b6184e7105c0efdf7953ad609e777629e2d
Yani Guan <56317222+veritas496@users.noreply.github.com>
2022-07-13 10:04:08 +0800

    Update dflow_3steps_cp2k_slurm.py

--- dflow_3steps_cp2k_slurm.py
+++ dflow_3steps_cp2k_slurm.py
@@ -39,7 +39,7 @@ class CP2KOpt(OP):
     def execute(self, op_in: OPIO) -> OPIO:
         cwd = os.getcwd()
         os.chdir(op_in["Opt_input"]) 
-        cmd = "ulimit -s unlimited; srun /opt/cp2k-9.1/cp2k-9.1-Linux-x86_64.ssmp -i input.inp -o output.out"
+        cmd = "your cp2k exec environment"
         subprocess.call(cmd, shell=True)
         os.chdir(cwd)        
         return OPIO({
@@ -66,7 +66,7 @@ class CP2KSingle(OP):
     def execute(self, op_in: OPIO) -> OPIO:
         cwd = os.getcwd()
         os.chdir(op_in["Single_input"])
-        cmd = "ulimit -s unlimited; srun /opt/cp2k-9.1/cp2k-9.1-Linux-x86_64.ssmp -i input.inp -o output.out"
+        cmd = "your cp2k exec environment"
         subprocess.call(cmd, shell=True)
         os.chdir(cwd)
         return OPIO({
@@ -75,23 +75,23 @@ class CP2KSingle(OP):
 
 def main():
     slurm_remote_executor = SlurmRemoteExecutor(
-    host="39.99.247.57", 
-    port=22, 
-    username="tutor",  
-    password="Bohrium#chengdu", 
-    header="#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks-per-node=4\n#SBATCH --partition=cpu\n#SBATCH -e test.err\n", 
+    host="your host", 
+    port=port, 
+    username="your username",  
+    password="your password", 
+    header="#!/bin/bash\n#SBATCH --nodes=1\n#SBATCH --ntasks-per-node=128\n#SBATCH --partition=cpu\n#SBATCH -e test.err\n", 
     )
 
     Structure_Opt = Step(
         "Structure-Opt",
-        PythonOPTemplate(CP2KOpt, image="dptechnology/dflow"),
+        PythonOPTemplate(CP2KOpt),
         artifacts={"Opt_input": upload_artifact(["./cp2k_opt"])},
         executor=slurm_remote_executor,
     )
 
     CP2K_Electronic = Step(
         "CP2K-Electronic",
-        PythonOPTemplate(CP2KSingle, image="dptechnology/dflow", slices=Slices("{{item}}", input_artifact=["Single_input"], output_artifact=["Single_output"])),
+        PythonOPTemplate(CP2KSingle, slices=Slices("{{item}}", input_artifact=["Single_input"], output_artifact=["Single_output"])),
         artifacts={"Single_input": upload_artifact(["cp2k_elf", "cp2k_density"])}, # your input dir
         with_param=range(2),
         key="CP2KSingle-{{item}}",
